aab6a004666ce810828f0918a4a89df51a595f3a
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2024-11-19 16:22:18 +0200

    Update Products.js
    added labels and changed country to location of product

--- smart-stock-frontend/src/components/Products.js
+++ smart-stock-frontend/src/components/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
     const [products, setProducts] = useState([]);
     const [newProduct, setNewProduct] = useState({
         category: '',
-        country: '',
+        location: '',
         productName: '',
         quantity: 0,
         orderer: 'NONE',
@@ -60,7 +60,7 @@ const Products = () => {
         setEditProductId(product.id);
         setNewProduct({
             category: product.category,
-            country: product.country,
+            location: product.location,
             productName: product.productName,
             quantity: product.quantity,
             orderer: product.orderer,
@@ -73,7 +73,7 @@ const Products = () => {
     const resetForm = () => {
         setNewProduct({
             category: '',
-            country: '',
+            lcoation: '',
             productName: '',
             quantity: 0,
             orderer: 'NONE',
@@ -103,7 +103,7 @@ const Products = () => {
                     <tr>
                         <th>ID</th>
                         <th>Kategooria</th>
-                        <th>Riik</th>
+                        <th>Asukoht</th>
                         <th>Toote nimi</th>
                         <th>Kogus</th>
                         <th>Kuupäev</th>
@@ -116,7 +116,7 @@ const Products = () => {
                         <tr key={product.id}>
                             <td>{product.id}</td>
                             <td>{product.category}</td>
-                            <td>{product.country}</td>
+                            <td>{product.location}</td>
                             <td>{product.productName}</td>
                             <td>{product.quantity}</td>
                             <td>{new Date(product.date).toLocaleDateString()}</td>
@@ -143,44 +143,65 @@ const Products = () => {
 
                 {showForm && (
                     <form onSubmit={saveProduct}>
-                        <input
-                            type="text"
-                            placeholder="Kategooria"
-                            value={newProduct.category}
-                            onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
-                            required
-                        />
-                        <input
-                            type="text"
-                            placeholder="Riik"
-                            value={newProduct.country}
-                            onChange={(e) => setNewProduct({ ...newProduct, country: e.target.value })}
-                            required
-                        />
-                        <input
-                            type="text"
-                            placeholder="Toote nimi"
-                            value={newProduct.productName}
-                            onChange={(e) => setNewProduct({ ...newProduct, productName: e.target.value })}
-                            required
-                        />
-                        <input
-                            type="number"
-                            placeholder="Kogus"
-                            value={newProduct.quantity}
-                            onChange={(e) =>
-                                setNewProduct({ ...newProduct, quantity: parseInt(e.target.value) })
-                            }
-                            required
-                        />
-                        <input
-                            type="text"
-                            placeholder="Lisainfo"
-                            value={newProduct.additionalInfo}
-                            onChange={(e) =>
-                                setNewProduct({ ...newProduct, additionalInfo: e.target.value })
-                            }
-                        />
+                        <div className="input-container">
+                            <label htmlFor="category">Kategooria</label>
+                            <input
+                                id="category"
+                                type="text"
+                                placeholder="Kategooria"
+                                value={newProduct.category}
+                                onChange={(e) => setNewProduct({...newProduct, category: e.target.value})}
+                                required
+                            />
+                        </div>
+
+                        <div className="input-container">
+                            <label htmlFor="location">Lokaadi (e.g., A1 - Shelf 1)</label>
+                            <input
+                                id="location"
+                                type="text"
+                                placeholder="Lokaadi (e.g., A1 - Shelf 1)"
+                                value={newProduct.location}
+                                onChange={(e) => setNewProduct({...newProduct, location: e.target.value})}
+                                required
+                            />
+                        </div>
+
+                        <div className="input-container">
+                            <label htmlFor="productName">Toote nimi</label>
+                            <input
+                                id="productName"
+                                type="text"
+                                placeholder="Toote nimi"
+                                value={newProduct.productName}
+                                onChange={(e) => setNewProduct({...newProduct, productName: e.target.value})}
+                                required
+                            />
+                        </div>
+
+                        <div className="input-container">
+                            <label htmlFor="quantity">Kogus</label>
+                            <input
+                                id="quantity"
+                                type="number"
+                                placeholder="Kogus"
+                                value={newProduct.quantity}
+                                onChange={(e) => setNewProduct({...newProduct, quantity: parseInt(e.target.value)})}
+                                required
+                            />
+                        </div>
+
+                        <div className="input-container">
+                            <label htmlFor="additionalInfo">Lisainfo</label>
+                            <input
+                                id="additionalInfo"
+                                type="text"
+                                placeholder="Lisainfo"
+                                value={newProduct.additionalInfo}
+                                onChange={(e) => setNewProduct({...newProduct, additionalInfo: e.target.value})}
+                            />
+                        </div>
+
                         <button type="submit">{isEditing ? 'Uuenda toodet' : 'Lisa toode'}</button>
                         <button type="button" onClick={resetForm}>Tühista</button>
                     </form>
